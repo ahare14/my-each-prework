6c0d83995d94ccfe2f970747e76161f58449abac
Alex Hare <alexanderhare14@gmail.com>
2019-04-09 22:01:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(array)
+  if block_given?
+    i = 0
+  while i < array.length
+    yield array[i]
+    puts i
+  end
+  array
+else
+  false
+  end
   # code here
 end
